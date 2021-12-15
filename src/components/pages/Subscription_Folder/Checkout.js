f0d1b9190c22a75ade9213dcb2598c472304cadb
Mackmiller <mackmiller19@gmail.com>
2021-12-14 21:52:39 -0500

    cleaned up code in Checkout.js and added comments

--- src/components/pages/Subscription_Folder/Checkout.js
+++ src/components/pages/Subscription_Folder/Checkout.js
@@ -9,89 +9,94 @@ const stripePromise = loadStripe("pk_test_51K6PZKAPJKSXew76gFW4UmquGYQXmllbtBUGo
 
 //message to appear if card payment is successful
 const successMessage = () => {
-  return (
-    <div className="success-msg">
-      <svg width="100px" height="100px" viewBox="0 0 16 16" className="bi bi-check2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-        <path fillRule="evenodd" d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
-      </svg>
-      <div className="title">Payment Successful</div>
-      <h6>Return to Profile</h6>
-    </div>
+    return (
+        <div className="success-msg">
+            <svg width="100px" height="100px" viewBox="0 0 16 16" className="bi bi-check2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fillRule="evenodd" d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
+            </svg>
+            <div className="title">Payment Successful</div>
+            <h6>Return to Profile</h6>
+        </div>
   )
 }
  
-// styled cart of items to show next to payment information
+// styled cart of items 
+// show next to payment information using bootstrap
 const cart = () => {
-  return (
-    <React.Fragment>
-       <h4 className="d-flex justify-content-between align-items-center mb-3">
-      <span className="text-muted">Your cart</span>
-      <span className="badge bg-secondary badge-pill">3</span>
-    </h4>
-    <ul className="list-group mb-3">
-      <li className="list-group-item d-flex justify-content-between lh-condensed">
-        <div>
-          <h6 className="my-0">First artwork</h6>
-          <small className="text-muted">Brief description</small>
-        </div>
-        <span className="text-muted">$200</span>
-      </li>
-      <li className="list-group-item d-flex justify-content-between lh-condensed">
-        <div>
-          <h6 className="my-0">Second artwork</h6>
-          <small className="text-muted">Brief description</small>
-        </div>
-        <span className="text-muted">$200</span>
-      </li>
-      <li className="list-group-item d-flex justify-content-between lh-condensed">
-        <div>
-          <h6 className="my-0">Third artwork</h6>
-          <small className="text-muted">Brief description</small>
-        </div>
-        <span className="text-muted">$200</span>
-      </li>
-      <li className="list-group-item d-flex justify-content-between bg-light">
-        <div className="text-success">
-          <h6 className="my-0">Promo code</h6>
-          <small>NEWYEAR2022</small>
-        </div>
-        <span className="text-success">-$25</span>
-      </li>
-      <li className="list-group-item d-flex justify-content-between">
-        <span>Total (USD)</span>
-        <strong>$575</strong>
-      </li>
-    </ul>
-    </React.Fragment>
-  )
+    return (
+        <React.Fragment>
+            <h4 className="d-flex justify-content-between align-items-center mb-3">
+            <span className="text-muted">Your cart</span>
+            <span className="badge bg-secondary badge-pill">3</span>
+            </h4>
+            <ul className="list-group mb-3">
+                <li className="list-group-item d-flex justify-content-between lh-condensed">
+                    <div>
+                    <h6 className="my-0">First artwork</h6>
+                    <small className="text-muted">Brief description</small>
+                    </div>
+                    <span className="text-muted">$200</span>
+                </li>
+                <li className="list-group-item d-flex justify-content-between lh-condensed">
+                    <div>
+                    <h6 className="my-0">Second artwork</h6>
+                    <small className="text-muted">Brief description</small>
+                    </div>
+                    <span className="text-muted">$200</span>
+                </li>
+                <li className="list-group-item d-flex justify-content-between lh-condensed">
+                    <div>
+                    <h6 className="my-0">Third artwork</h6>
+                    <small className="text-muted">Brief description</small>
+                    </div>
+                    <span className="text-muted">$200</span>
+                </li>
+                <li className="list-group-item d-flex justify-content-between bg-light">
+                    <div className="text-success">
+                    <h6 className="my-0">Promo code</h6>
+                    <small>NEWYEAR2022</small>
+                    </div>
+                    <span className="text-success">-$25</span>
+                </li>
+                <li className="list-group-item d-flex justify-content-between">
+                    <span>Total (USD)</span>
+                    <strong>$575</strong>
+                </li>
+            </ul>
+        </React.Fragment>
+    )
 }
  
-// checkout page structure
+// checkout component structure for route render
 function Checkout() {
-  const [paymentCompleted, setPaymentCompleted] = useState(false);
- 
-  return (
-    <div className="container">
-      <div className="py-5 text-center">
-        <h4>Stripe Test</h4>
-      </div>
- 
-      <div className="row s-box">
-        {paymentCompleted ? successMessage() : 
-        <React.Fragment>
-          <div className="col-md-5 order-md-2 mb-4">
-            {cart()}
-          </div>
-          <div className="col-md-7 order-md-1">
-            <Elements stripe={stripePromise}>
-              <CheckoutForm amount={575} setPaymentCompleted={setPaymentCompleted} />
-            </Elements>
-          </div>
-        </React.Fragment>}
-      </div>
- 
-    </div>
-  );
+    // boolean for testing successful payment
+    const [paymentCompleted, setPaymentCompleted] = useState(false);
+    // return success message or CheckoutForm component
+    return (
+        <div className="container">
+            <div className="py-5 text-center">
+                <h4>Checkout</h4>
+            </div>
+            <div className="row s-box">
+                {/* if payment has been completed, show the success message */}
+                {paymentCompleted ? successMessage() : 
+                // if not, show the cart of artwork/shipping/billing form:
+                    <React.Fragment>
+                        <div className="col-md-5 order-md-2 mb-4">
+                            {/* pulled in cart from above */}
+                            {cart()}
+                        </div>
+                        <div className="col-md-7 order-md-1">
+                            <Elements stripe={stripePromise}>
+                                {/* pass bill amount and setpaymentcompleted boolean down to CheckoutForm */}
+                                <CheckoutForm amount={575} setPaymentCompleted={setPaymentCompleted} />
+                            </Elements>
+                        </div>
+                    </React.Fragment>
+                }
+            </div>
+        </div>
+    );
 }
  
 export default Checkout;
