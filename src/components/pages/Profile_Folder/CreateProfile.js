c3f2dfd4119689c4483e9c0c6bcb26c3565f98f7
mgkdn9 <mkohlberg95@gmail.com>
2021-12-15 09:00:59 -0600

    got db tags to show on profile creation page and checkboxes addtags to newProfile object

--- src/components/pages/Profile_Folder/CreateProfile.js
+++ src/components/pages/Profile_Folder/CreateProfile.js
@@ -1,19 +1,38 @@
 import React, { useState, useEffect } from 'react'
+import { scryRenderedDOMComponentsWithTag } from 'react-dom/test-utils'
 import Tag from '../../Tag'
 
 const CreateProfile = (props) => {
 
-  const [newProfile, setNewProfile] = useState({})
+  const [newProfile, setNewProfile] = useState({
+    //Other stuff will go in this object but basically we need to declare a property called tags as an array so that the spread operator will work in the first call of handleCheck
+    tags:[]
+  })
+  const [tags, setTags] = useState([])
+
+  useEffect(() => {
+    getTags()
+  }, [])
 
   const handleChange = e => {
     setNewProfile({...newProfile, [e.target.name]:e.target.value})
   }
   const handleCheck = e => {
-    // setNewProfile({...newProfile, tags.})
+    setNewProfile({...newProfile, tags:[...newProfile.tags, e.target.id]})
   }
 
-  const allTags = () => {
+  useEffect(() => {//Delete after form works
+    console.log('newProfile:\n',newProfile)
+  }, [newProfile])
 
+  const getTags = () => {
+    fetch('http://localhost:8000/tags')
+      .then(res => res.json())
+      .then(foundTags => {
+          console.log('Found Tags by INDEX', foundTags.tags)
+          setTags(foundTags.tags)
+      })
+      .catch(err => console.log(err))
   }
 
   return (
@@ -22,21 +41,26 @@ const CreateProfile = (props) => {
       <form>
         <div>
           <label htmlFor="name">Name:</label>
-          <input onChange={handleChange} type="text" name="name" id="name"/>
+          <input required onChange={handleChange} type="text" name="name" id="name"/>
         </div>
         <div>
           <label htmlFor="address">Address:</label>
-          <input onChange={handleChange} type="text" name="address" id="address"/>
+          <input required onChange={handleChange} type="text" name="address" id="address"/>
         </div>
-        <div>
+        {/* <div>
           <label htmlFor="creditCard">Credit Card #:</label>
           <input onChange={handleChange} type="number" name="creditCard" id="creditCard"/>
-        </div>
+        </div> */}
         <div>
           <h2>Favorite Categories</h2>
-          {/* {allTags} This is just to get started */}
-          <label htmlFor="surrealism">Surrealism:</label>
-          <input onChange={handleCheck} type="checkbox" name="surrealism" id="surrealism" />
+          {
+            tags.map(tag => (
+              <li>
+                <label htmlFor={tag.name}>{tag.name}</label>
+                <input onChange={handleCheck} type="checkbox" name={tag.name} id={tag._id} />
+              </li>
+            ))
+          }
         </div>
       </form>
     </div>
