8df05d031f54bc97a41ca959d78447f659c67567
Mackmiller <mackmiller19@gmail.com>
2021-12-18 00:19:32 -0500

    additional styling to plans, community, and home.js for responsiveness and clarity

--- src/components/homeComponents/Community.js
+++ src/components/homeComponents/Community.js
@@ -1,14 +1,21 @@
 import React from "react"
 import { Card, CardGroup } from "react-bootstrap"
 import CommunityImage from "./../homeComponents/images/community1.jpeg"
+import CommunityImage2 from "./../homeComponents/images/community2.jpg"
 
 
 const communitystyle = {
-    height: '30rem',
-    width: '40rem',
+    height: '400px',
+    objectFit: 'cover'
+}
+
+const communitystyle2 = {
+    height: '400px',
+    objectFit: 'cover'
 }
 
 const space = {
+    textAlign: "center",
     margin: '45px'
 }
 const Community = () => {
@@ -23,13 +30,22 @@ const Community = () => {
                 <Card>
                     <Card.Body>
                         <Card.Text>
-                            "Lorem ipsum dolor sit amet, cibo sensibus interesset no sit. Et dolor possim volutpat qui. No malis tollit iriure eam, et vel tale zril blandit, rebum vidisse nostrum qui eu."
+                            "I never have to stand in the checkout line at HomeGoods ever again!"
+                        </Card.Text>
+                        <Card.Text>
+                            "I love switching out my artwork each season, and I don't even have to leave my house."
+                        </Card.Text>
+                        <Card.Text>
+                            "I started out with the lowest tier of artwork delivery (1 per season) and now I am receiving 8 per season and couldn't be happier that my house is turning into an art museum!"
+                        </Card.Text>
+                        <Card.Text>
+                            "I no longer need an interior designer to handle my artwork for me."
                         </Card.Text>
                     </Card.Body>
                 </Card>
                 <br />
                 <Card>
-                    <Card.Img variant="top" src={CommunityImage} style={communitystyle} />
+                    <Card.Img variant="top" src={CommunityImage2} style={communitystyle2} />
                 </Card>
             </CardGroup>
         </div>
--- src/components/homeComponents/Plans.js
+++ src/components/homeComponents/Plans.js
@@ -6,11 +6,27 @@ import PlansImage from "./../homeComponents/images/plansImage.jpeg"
 
 
 const cardstyle = {
-    margin: '25px'
+    margin: '20px auto',
+    objectFit: 'cover',
+    border: "none"
+
 }
 
 const text = {
-    fontSize: '60px'
+    fontSize: '40px',
+    width: "400px",
+    marginTop: '40px'
+}
+
+const text2 = {
+  fontSize: '15px',
+  width: "400px",
+  marginTop: '20px',
+  marginBottom: '20px'
+}
+
+const image = {
+  minWidth: "400px"
 }
 
 const PlansSection = () => {
@@ -23,21 +39,26 @@ const handleClick = (e) =>{
 }
 return (
 <div>
-    <CardGroup>
-  <Card style={cardstyle}>
-    <Card.Img variant="top" src={PlansImage} />
-  </Card>
-  <br />
-  <Card style={cardstyle}>
-  <Card.Body>
-    <Card.Title style={text}>Explore Our Memberships</Card.Title>
-    <Card.Text>
-      With supporting text below as a natural lead-in to additional content.
-    </Card.Text>
-    <Button variant="dark" onClick={handleClick}>Learn More</Button>
-  </Card.Body>
-</Card>
-</CardGroup> 
+    <div className="row">
+      <div className="col">
+      <Card style={cardstyle}>
+        <Card.Img variant="top" src={PlansImage} style={image} />
+      </Card>
+      </div>
+      <br />
+      <div className="col">
+      <Card style={cardstyle}>
+      <Card.Body>
+        <Card.Title style={text}>Explore Our Subscriptions</Card.Title>
+        <Card.Text style={text2}>
+          We have a plan for every budget, tiered by the number of art pieces delivered each season.
+        </Card.Text>
+        <Button variant="dark" onClick={handleClick}>Learn More</Button>
+       </Card.Body>
+      </Card>
+      </div>
+    </div>
+
 </div>
 )
 }
