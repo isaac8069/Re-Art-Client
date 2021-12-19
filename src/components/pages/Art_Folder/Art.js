1578ed666371ca04b2e91e8bdd12d06328196148
Mackmiller <mackmiller19@gmail.com>
2021-12-19 00:20:29 -0500

    added styling to general art collection page

--- src/components/pages/Art_Folder/Art.js
+++ src/components/pages/Art_Folder/Art.js
@@ -1,6 +1,20 @@
 import React, { useState, useEffect } from 'react'
 // Import Pieces to show each art piece
-import Pieces from './Pieces'
+import Pieces from './Pieces_All'
+
+const title = {
+    fontSize: '40px',
+    textAlign: 'center',
+    margin: '38px 0px 20px 0px'
+  }
+  
+  const subtitle = {
+    fontSize: '20px',
+    textAlign: 'center',
+    width: "720px",
+    margin: "0 auto",
+    paddingBottom: "20px"
+  }
 
 const Art = (props) => {
 
@@ -24,13 +38,19 @@ const Art = (props) => {
             artist = {a.artist}
             imgUrl = {a.imgUrl}
             description = {a.description}
-            price = {a.price}
+            // price = {a.price}
         />
     })
 
     return (
         <div>
-            {pieces}
+            <h1 style={title}>
+				The Collection
+			</h1>
+            <p style={subtitle}>Browse a portion of our collection below- the possibilities are endless! Once you create a profile, you can list keywords to begin filtering the artwork by style.</p>
+            <div className = "row">            
+                {pieces}
+            </div>
         </div>
     )
 }
