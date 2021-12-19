740d51da14d447c862e406211a803c1d6f625b6e
Mackmiller <mackmiller19@gmail.com>
2021-12-19 01:09:43 -0500

    cleaned code spacing for profile folder

--- src/components/pages/Profile_Folder/CreateProfile.js
+++ src/components/pages/Profile_Folder/CreateProfile.js
@@ -10,27 +10,22 @@ const box = {
   margin: '2px',
   padding: '5px'
 }
-
 const button = {
   margin: '10px',
 }
-
 const bgc = {
   backgroundColor: 'lightgrey',
   marginTop: "20px",
   padding: '25px'
 }
-
 const title = {
   fontSize: '40px',
   textAlign: 'left',
   margin: '20px'
 }
-
 const subtitle = {
 	fontSize: '20px',
 }
-
 const list = {
   listStyle: 'none'
 }
@@ -41,7 +36,6 @@ const CreateProfile = (props) => {
 
   // Setting a state to hold our users newProfile that will be sent to data Base to be stored with usersId as reference
   // Also setting a state for our tags
-
   const [newProfile, setNewProfile] = useState({
     //Other stuff will go in this object but basically we need to declare a property called tags as an array so that the spread operator will work in the first call of handleCheck
     tags: [],
@@ -51,21 +45,18 @@ const CreateProfile = (props) => {
   const [tags, setTags] = useState([])
 
   // useEffect that calls getTags everytime the component renders
-
   useEffect(() => {
     getTags()
   }, [])
 
   // Function that runs everytime that a input for either name or address changes
   // Function sets all inputs to our newProfile state
-
   const handleChange = e => {
     setNewProfile({ ...newProfile, [e.target.name]: e.target.value })
   }
 
   // This function will run everytime one of the profile checkboxes change
   // Based on checked stats of the checkbox will either add or remove the targeted tag to newProfile
-
   const handleCheck = e => {
     if (e.target.checked) {
       setNewProfile({ ...newProfile, tags: [...newProfile.tags, e.target.id] })
@@ -79,7 +70,6 @@ const CreateProfile = (props) => {
   }
 
   // this is the API call for tags at end of funciton sets found tags to our tag state
-
   const getTags = () => {
     fetch('http://localhost:8000/tags')
       .then(res => res.json())
@@ -94,7 +84,6 @@ const CreateProfile = (props) => {
   // This funciton is set to run once the submit profile button is pressed
   // This sets the newProfile state to a object that is then sent to our Data Base as a POST request
   // At the end of function getProfile and patchProfile are run to ensure that profile data in App.js is up to date
-
   const postProfile = (e) => {
     e.preventDefault()
     console.log('Pressed Submit button')
@@ -114,10 +103,10 @@ const CreateProfile = (props) => {
       },
     }
     fetch('http://localhost:8000/profiles', requestOptions)
-    .then(postedProfile=> {
-      props.getProfile()
-      navigate('/')
-    })
+      .then(postedProfile=> {
+        props.getProfile()
+        navigate('/')
+      })
       .catch(err => console.error(err))
   }
 
@@ -132,14 +121,12 @@ const CreateProfile = (props) => {
               <Form.Control style={{ width: '18rem' }} placeholder="Enter name" onChange={handleChange} type="text" name="name" id="name" />
             </Form.Group>
           </div>
-
           <div className='container' style={box}>
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label style={subtitle}>Address</Form.Label>
               <Form.Control style={{ width: '18rem' }} placeholder="Address" onChange={handleChange} type="text" name="address" id="address" />
             </Form.Group>
           </div>
-
           <div className='container' style={box}>
             <Card style={{ width: '18rem' }}>
               <Card.Header style={subtitle}>Favorites</Card.Header>
@@ -156,12 +143,10 @@ const CreateProfile = (props) => {
           <Button variant="light" type="submit" style={button}>
             Submit
           </Button>
-
         </Form>
       </div>
     </div>
   )
 }
-
 
 export default CreateProfile
