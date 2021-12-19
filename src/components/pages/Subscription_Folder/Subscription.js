ae31297059a37dea061cee1755c20a94768054e3
Mackmiller <mackmiller19@gmail.com>
2021-12-19 01:01:47 -0500

    cleaned spacing in code for subscription.js

--- src/components/pages/Subscription_Folder/Subscription.js
+++ src/components/pages/Subscription_Folder/Subscription.js
@@ -5,7 +5,7 @@ import SignIn from '../../auth/SignIn'
 import messages from '../../shared/AutoDismissAlert/messages'
 import { Card, Button, Row, Col, CardGroup } from 'react-bootstrap'
 
-
+// styling
 const button = {
   margin: '20px',
 }
@@ -46,7 +46,6 @@ function Subscription(props) {
   // tests if user is populated
   // if populated will go onto next test for profile
   // if passes all tests will enter checkout page
-
   const handleClick = (e) => {
     // Check for signed in
     if (props.user === null) {
@@ -101,7 +100,6 @@ function Subscription(props) {
             </Card.Body>
           </Card>
           <br />
-
           <Card border="dark" style={{ width: '18rem', margin: '20px', backgroundColor: "#D3D3D3", border: "none" }}>
             <Card.Body>
               <Card.Title>Value Access</Card.Title>
@@ -117,7 +115,6 @@ function Subscription(props) {
             </Card.Body>
           </Card>
           <br />
-
           <Card border="dark" style={{ width: '18rem', margin: '20px', backgroundColor: "#D3D3D3", border: "none" }}>
             <Card.Body>
               <Card.Title>Premium Access</Card.Title>
@@ -134,7 +131,6 @@ function Subscription(props) {
           </Card>
         </Row>
       </div>
-
       <div style={iconBoxesContainer}>
         <Row xs={1} md={2} className="g-4" style={iconBoxesContainer}>
           <Card style={{width: "400px", margin: "25px" }} >
@@ -160,6 +156,17 @@ function Subscription(props) {
             </Card.Body>
           </Card>
           <Card style={{width: "400px", margin: "25px" }} >
+            <svg style={icon} xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+              <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+            </svg>
+            <Card.Body>
+              <Card.Title>Members-Only Sales</Card.Title>
+              <Card.Text>
+                Get exclusive discounts on purchases.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card style={{width: "400px", margin: "25px" }} >
             <svg style={icon} xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-receipt" viewBox="0 0 16 16">
               <path d="M1.92.506a.5.5 0 0 1 .434.14L3 1.293l.646-.647a.5.5 0 0 1 .708 0L5 1.293l.646-.647a.5.5 0 0 1 .708 0L7 1.293l.646-.647a.5.5 0 0 1 .708 0L9 1.293l.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .801.13l.5 1A.5.5 0 0 1 15 2v12a.5.5 0 0 1-.053.224l-.5 1a.5.5 0 0 1-.8.13L13 14.707l-.646.647a.5.5 0 0 1-.708 0L11 14.707l-.646.647a.5.5 0 0 1-.708 0L9 14.707l-.646.647a.5.5 0 0 1-.708 0L7 14.707l-.646.647a.5.5 0 0 1-.708 0L5 14.707l-.646.647a.5.5 0 0 1-.708 0L3 14.707l-.646.647a.5.5 0 0 1-.801-.13l-.5-1A.5.5 0 0 1 1 14V2a.5.5 0 0 1 .053-.224l.5-1a.5.5 0 0 1 .367-.27zm.217 1.338L2 2.118v11.764l.137.274.51-.51a.5.5 0 0 1 .707 0l.646.647.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.509.509.137-.274V2.118l-.137-.274-.51.51a.5.5 0 0 1-.707 0L12 1.707l-.646.647a.5.5 0 0 1-.708 0L10 1.707l-.646.647a.5.5 0 0 1-.708 0L8 1.707l-.646.647a.5.5 0 0 1-.708 0L6 1.707l-.646.647a.5.5 0 0 1-.708 0L4 1.707l-.646.647a.5.5 0 0 1-.708 0l-.509-.51z" />
               <path d="M3 4.5a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm8-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5z" />
@@ -171,21 +178,9 @@ function Subscription(props) {
               </Card.Text>
             </Card.Body>
           </Card>
-          <Card style={{width: "400px", margin: "25px" }} >
-            <svg style={icon} xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-              <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
-            </svg>
-            <Card.Body>
-              <Card.Title>Members-Only Sales</Card.Title>
-              <Card.Text>
-                Get exclusive discounts on purchases.
-              </Card.Text>
-            </Card.Body>
-          </Card>
 
         </Row>
       </div>
-
       <div className='container'>
         <h2 style={box}>How it Works</h2>
         <CardGroup className=''>
@@ -205,8 +200,6 @@ function Subscription(props) {
           </Card>
         </CardGroup>
       </div>
-
-
     </div>
   )
 }
